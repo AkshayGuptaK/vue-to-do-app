559315064efcb860b5c2cdc3b79f0e60b9b72502
Akshay Gupta <akshaygrkesh@gmail.com>
2018-11-06 16:08:14 +0530

    Task Container templates

---
+++ components/CompletedTasks.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <div class="divider"></div>
+        <h2>Completed</h2>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+p {
+    text-align: center;
+    font-size: 1.5rem;
+}
+
+.divider {
+    margin: auto;
+    width: 80%;
+    padding-top: 10px;
+    border-bottom: solid blue 1px;
+}
+</style>
---
+++ components/CurrentTasks.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <p v-if="empty" >Twiddling my thumbs, nothing to do.</p>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+p {
+    text-align: center;
+    font-size: 1.5rem;
+}
+</style>
