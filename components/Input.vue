8b7070c93398d3e8a7cb348c4932e24baa01f8c9
Akshay Gupta <akshaygrkesh@gmail.com>
2018-11-06 15:35:58 +0530

    Template and styling of task and input components

---
+++ components/Input.vue
@@ -0,0 +1,47 @@
+<template>
+    <form>
+        <input type="text" id="inputTaskName" name="TaskName" placeholder="Enter a Task">
+        <button title="Add Task" type="submit"></button>
+        <input type="text" id="inputTaskDescription" name="TaskDescription" placeholder="Enter a Description">
+    </form>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+form {
+    margin: 20px;
+    display: table;
+    width: 100%;
+}
+
+input {
+    display: table-cell;
+    vertical-align: middle;
+    width: 95%;
+    min-width: 100px;
+    background-color: beige;
+    border-radius: 5px;
+}
+
+#inputTaskName {
+    font-size: 1.5rem;
+    margin: 0px 5px 5px 0px;
+}
+
+#inputTaskDescription {
+    font-size: 1rem;
+    margin-right: 5px;
+}
+
+button {
+    background: url('images/add.png') no-repeat local;
+    background-size: 100% 100%;
+    padding: 11px;
+    border-radius: 5px;
+    display: table-cell;
+    vertical-align: middle;
+    margin: 0px 0px 5px 0px;
+}
+</style>
---
+++ components/Task.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="task" :taskId = id completed=true>
+    <input type="text" disabled=true class="taskname" :value="name">
+    <input type="text" disabled=true class="taskdesc" :value="description">
+    <button title="Edit Task" class="editBtn" @click="editTask"></button>
+    <button title="Describe Task" class="describeBtn" @click="describeTask"></button>
+    <button title="Delete Task" class="deleteBtn" @click="deleteTask"></button>
+    <button title="Task Completed" class="completeBtn" @click="completeTask"></button>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+.task {
+    margin: 20px;
+    display: grid;
+    grid-template-columns: 1fr 30px 30px;
+    grid-template-rows: 5fr 1fr 4fr;
+    grid-column-gap: 5px;
+    width: 95.4%;
+}
+
+input {
+    background-color: rgba(245, 245, 220, 0.8);
+    border: none;
+    border-radius: 5px;
+    color: grey;
+    width: 99.5%;
+    min-width: 75px;
+    padding: 5px 0px 5px 5px;
+    text-overflow: ellipsis;
+}
+
+.taskname {
+    font-size: 1.5rem;
+    margin: 0px 0px 5px 0px;
+    grid-area: 1 / 1 / 3 / 2;
+}
+
+.taskdesc {
+    font-size: 1rem;
+    margin: 0px;
+    grid-area: 3 / 1 / 4 / 2;
+}
+
+input[disabled] {
+    color: black;
+}
+
+button:hover {
+    background-color: lightblue;
+}
+
+.deleteBtn {
+    background: url('images/delete.png') no-repeat local;
+    background-size: 100% 100%;
+    grid-area: 2 / 3 / 4 / 4;
+}
+
+.editBtn {
+    background: url('images/edit.png') no-repeat local;
+    background-size: 100% 100%;
+    grid-area: 1 / 2 / 2 / 3;
+}
+
+.describeBtn {
+    background: url('images/describe.png') no-repeat local;
+    background-size: 100% 100%;
+    grid-area: 2 / 2 / 4 / 3;
+}
+
+.completeBtn {
+    background: url('images/complete.png') no-repeat local;
+    background-size: 100% 100%;
+    grid-area: 1 / 3 / 2 / 4;
+}
+
+.incompleteBtn {
+    background: url('images/incomplete.png') no-repeat local;
+    background-size: 100% 100%;
+    grid-area: 1 / 3 / 2 / 4;
+}
+
+.acceptNameEditBtn, .acceptDescEditBtn {
+    background: url('images/apply-change.png') no-repeat local;
+    background-size: 100% 100%;
+}
+
+.acceptNameEditBtn {
+    grid-area: 1 / 2 / 2 / 3;
+}
+
+.acceptDescEditBtn {
+    grid-area: 2 / 2 / 4 / 3;
+}
+</style>
