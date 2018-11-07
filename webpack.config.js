7ca0003bb7f5898632a7c7828a843447ac7df78c
Akshay Gupta <akshaygrkesh@gmail.com>
2018-11-07 12:36:53 +0530

    Webpack compilation now working

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const path = require('path')
+const { VueLoaderPlugin } = require('vue-loader')
+
+module.exports = {
+   module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+        },
+        {
+         test: /\.vue$/,
+         loader: 'vue-loader'
+       }
+      ]
+    },
+    resolve: {
+      alias: {
+        'vue$': 'vue/dist/vue.esm.js'
+      },
+      extensions: ['*', '.js', '.vue', '.json']
+    },
+    plugins: [
+        new VueLoaderPlugin()
+    ]
+}
