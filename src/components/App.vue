a0c89bacf86c71e458ee41ca19c869a1ccd0a881
Akshay Gupta <akshaygrkesh@gmail.com>
2018-11-08 11:54:36 +0530

    Main App component file

---
+++ src/components/App.vue
@@ -0,0 +1,23 @@
+<template>
+	<div id="app">
+		<Input/>
+	</div>
+</template>
+
+<script>
+import Input from './Input.vue'
+
+export default {
+	components: {
+		Input
+	},
+	methods: {
+		addTask (name, desc) {
+			console.log(name, desc) // placeholder
+		}
+	}
+}
+</script>
+
+<style>
+</style>
